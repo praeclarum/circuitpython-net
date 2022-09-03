831134c74d75f10c5f22d873cdda4e069c7acb5a
Frank A. Krueger <fak@praeclarum.org>
2022-09-03 11:31:07 -0700

    Update example

--- example/Program.cs
+++ example/Program.cs
@@ -1,5 +1,9 @@
-﻿
+﻿using CircuitPython;
+
 var engine = new CircuitPython.Engine();
 
-engine.Execute("print('Hello, world! -from .NET')", CircuitPython.InputKind.Eval);
-engine.ExecuteRepl();
+engine.Execute("print('Hello, world!')", InputKind.Eval);
+
+var result = engine.Execute("2 + 3", InputKind.Eval);
+
+Console.WriteLine(result?.Int64Value);
